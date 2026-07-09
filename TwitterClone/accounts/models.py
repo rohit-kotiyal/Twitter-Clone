34973a2291a27df2e56b92922a71e0d7bf7351cd
Rohit Kotiyal <rohitkotiyal96@gmail.com>
2026-07-09 17:17:01 +0530

    Initial setup: project structure, accounts app, Profile model with signal

---
+++ TwitterClone/accounts/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# Create your models here.
+
+class Profile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)   #OPTIONAL
+    location = models.CharField(max_length=100, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='accounts/profile_picture', blank=True, null=True)
+
+
+    def __str__(self):
+        return self.user.username
+    
+
+
+# Singnal
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
